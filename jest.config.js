1b1eb2d821e7b67c5cdc96eb584807d82195bf24
Renaud Bernier <renaudbernier@hotmail.fr>
2026-04-19 20:19:55 -0400

    fix(test): updated sanity test to remove import

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
   modulePathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/*.test.[jt]s?(x)"],
+  testMatch: ["**/*.test.[jt]s?(x)"],
   collectCoverageFrom: [
     "app/**/*.{js,jsx,ts,tsx}",
     "components/**/*.{js,jsx,ts,tsx}",
