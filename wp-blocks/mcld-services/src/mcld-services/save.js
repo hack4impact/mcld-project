feb58d24b6b08c54848d10552aa43b2d5958ca7c
Jia <77560326+Jxl-s@users.noreply.github.com>
2026-05-03 14:51:58 -0400

    feat: services fetched server-side + cached

--- wp-blocks/mcld-services/src/mcld-services/save.js
+++ wp-blocks/mcld-services/src/mcld-services/save.js
@@ -1,10 +1,3 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
-export default function save( { attributes } ) {
-	const { apiUrl } = attributes;
-	return (
-		<div { ...useBlockProps.save() } data-api-url={ apiUrl }>
-			Loading…
-		</div>
-	);
+export default function save() {
+	return null;
 }
