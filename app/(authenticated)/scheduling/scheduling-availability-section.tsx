4d1a953af29e1022b4deec831aa3eb373d5dafce
achneerov <achneerov@gmail.com>
2026-05-18 18:55:58 -0400

    chore(scheduling): remove unused SchedulingAvailabilitySection component

--- app/(authenticated)/scheduling/scheduling-availability-section.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { UserAvailabilityCalendarSubmitCoachMessage } from "@/components/scheduling/user-availability-calendar-submit-coach-message";
-
-export function SchedulingAvailabilitySection() {
-   return <UserAvailabilityCalendarSubmitCoachMessage />;
-}
